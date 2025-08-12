2cd1b2324aa55524c9f655520e54502ad8eb714e
KokebBenti <71655196+KokebBenti@users.noreply.github.com>
2025-08-12 12:28:56 +0300

    Update EDA.py

--- Scripts/EDA.py
+++ Scripts/EDA.py
@@ -7,7 +7,7 @@ def change_type(df,col):
  import pandas as pd
  df[col]=pd.to_datetime(df[col])
  return df   
- 
+
 def plot_numerical(df,col):
  import matplotlib.pyplot as plt
  df[col].hist(bins=50, figsize=(8,6))
@@ -26,8 +26,25 @@ def plot_categorical(df,col):
  plt.ylabel('Frequency')
  plt.show()   
 
+def correlation(df):
+ num_df = df.select_dtypes(include=['int64', 'float64']) 
+ m=num_df.corr()  
+ return m
+
 def box_plot(df,col):
  import matplotlib.pyplot as plt
  plt.boxplot(df[col])
  plt.title(col)
  plt.show() 
+
+
+def remove_outliers(df,col):
+ from scipy.stats import zscore
+ df['z_score'] = zscore(df[col])
+ df=df[abs(df['z_score']) < 3]
+ return df
+
+
+def to_int(df,col):
+ df[col] = df[col].astype(int)  
+ return df
